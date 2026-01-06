3d7a4cda92f1f40d08cc22ce91f215e9ba88664b
Aryan Singh <aoneone5@protonmail.com>
2026-01-07 00:39:11 +0530

    New layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,15 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   site: "https://a115.xyz",
-
+  output: "static",
+  adapter: cloudflare(),
   integrations: [sitemap()],
 });
+
